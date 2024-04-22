6ff40a046ed7fb873f1b29ef69aee8ba3196dbcc
unknown <21120508@student.hcmus.edu.vn>
2024-04-22 16:12:37 +0700

    using hivemq as broker

--- pub.py
+++ pub.py
@@ -1,34 +1,59 @@
-import paho.mqtt.client as mqtt
+import paho.mqtt.client as paho
 import time
+from paho import mqtt
 import cv2
-import random
+import base64
+import os
+import dotenv
 
-# Config
-mqtt_broker = "127.0.0.1"
-mqtt_port = 1883
+dotenv.load_dotenv()
+
 mqtt_topic = "test_cam"
-
-client = mqtt.Client()
+client = paho.Client()
 
 # Connect to broker
-client.connect(mqtt_broker, mqtt_port)
+#client.connect(mqtt_broker, mqtt_port)
+
+client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+# set username and password
+client.username_pw_set(os.getenv("USER"), os.getenv("PASSWORD"))
+#connect to HiveMQ Cloud on port 8883 (default for MQTT)
+client.connect(os.getenv("CLUSTER"),int(os.getenv("PORT")))
 
 # Webcam
 webcam = cv2.VideoCapture(0)
 
 while True:
-    # Read frame từ webcam
+    print("Sending...")    
+    # Read frame from webcam
     ret, frame = webcam.read()
+    if not ret:
+        print("Failed to read frame")
+        break
 
-    # Convert frame to binary
-    _, img_encoded = cv2.imencode('.jpg', frame)
-    message = img_encoded.tobytes()
+    # Resize frame
+    scale_percent = 50  
+    width = int(frame.shape[1] * scale_percent / 100)
+    height = int(frame.shape[0] * scale_percent / 100)
+    resized_frame = cv2.resize(frame, (width, height))
 
-    # Send message
-    client.publish(mqtt_topic, payload=message, qos=0)
+    # Print size (bytes)
+    print('Size of original: ', frame.itemsize * frame.size)
+    print('Size of resized: ', resized_frame.itemsize * resized_frame.size)
 
-    # Delay for reducing CPU usage
-    time.sleep(0.1)
+    # Encode parameters
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 50]  
+
+    # Encode frame to base64
+    retval, buffer = cv2.imencode('.jpg', resized_frame, encode_param)
+    jpg_as_text = base64.b64encode(buffer)
+
+    print('Size of message', len(jpg_as_text))
+    # Publish to MQTT
+    client.publish(mqtt_topic, jpg_as_text)
+
+    # Delay
+    time.sleep(0.2)
 
 # Close MQTT connection and webcam
 client.disconnect()
--- sub.py
+++ sub.py
@@ -1,10 +1,17 @@
-import paho.mqtt.client as mqtt
+import paho.mqtt.client as paho
 import cv2
+from paho import mqtt
+import base64
 import numpy as np
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
 # Config 
-mqtt_broker = "127.0.0.1"
-mqtt_port = 1883
 mqtt_topic = "test_cam"
 
 # Connect
@@ -14,21 +21,39 @@ def on_connect(client, userdata, flags, rc):
 
 # On message
 def on_message(client, userdata, msg):
-    nparr = np.frombuffer(msg.payload, np.uint8)
-    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    print("Received message")
+    print("Size of message", len(msg.payload))
+
+    # Decode base64
+    jpg_original = base64.b64decode(msg.payload)
+    jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
+    img = cv2.imdecode(jpg_as_np, flags=1)
+
+    # Resize
+    scale_percent = 200
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    img = cv2.resize(img, (width, height))
     
-    # Show image
-    cv2.imshow("Video", image)
-    cv2.waitKey(1)  
+    # Display
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+   
 
 # Init MQTT Client
-client = mqtt.Client()
+client = paho.Client()
+
 
 client.on_connect = on_connect
 client.on_message = on_message
 
-# Connect to Broker
-client.connect(mqtt_broker, mqtt_port, 60)
+
+# Set ssl protocol
+client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+# set username and password
+client.username_pw_set(os.getenv("USER"), os.getenv("PASSWORD"))
+#connect to HiveMQ Cloud on port 8883 (default for MQTT)
+client.connect(os.getenv("CLUSTER"),int(os.getenv("PORT")))
 
 # Listen to the topic
 client.loop_forever()
